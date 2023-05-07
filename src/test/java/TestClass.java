18eabe6501d7252a7b370682b5f99e7a495f9461
Mustafa <mezbb2023@gmail.com>
2023-05-07 17:28:29 +0300

    first commit

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,5 +1,5 @@
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Git den merhaba... ");
+        System.out.println("Git den mustfa merhaba... ");
     }
 }
